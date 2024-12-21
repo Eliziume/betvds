fd19efba0e891cf0572c86a1f54584c3db59a4db
Eliziume <130708263+Eliziume@users.noreply.github.com>
2024-12-21 03:27:07 +0300

    Add files via upload

---
+++ js/index.bundle.js
@@ -0,0 +1,1 @@
+!function(){const t=document.querySelector(".burger-btn"),e=document.querySelector(".nav"),c=document.querySelector("body");t.addEventListener("click",(function(){t.classList.toggle("active"),t.classList.toggle("not-active"),e.classList.toggle("active"),c.classList.toggle("lock")}))}();
